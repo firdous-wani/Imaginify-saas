eded3730f2f7819f7d7881d89164620863f199db
firdous-wani <azharwani7081@gmail.com>
2024-08-11 16:36:33 +0530

    Revert "upated clerk webhooks"
    This reverts commit d3c3675c40e8aa47d7dc90117023597ffe481890.

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 import { clerkClient } from "@clerk/nextjs/server";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
@@ -62,13 +61,13 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
-    const user = {
+    const user: CreateUserParams = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
+      firstName: first_name!,
+      lastName: last_name!,
+      photo: image_url!,
     };
 
     const newUser = await createUser(user);
@@ -89,11 +88,11 @@ export async function POST(req: Request) {
   if (eventType === "user.updated") {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
-      firstName: first_name,
-      lastName: last_name,
+    const user: UpdateUserParams = {
+      firstName: first_name!,
+      lastName: last_name!,
       username: username!,
-      photo: image_url,
+      photo: image_url!,
     };
 
     const updatedUser = await updateUser(id, user);
